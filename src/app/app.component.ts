915cfede57e4a875b4e3fa71fb7d3c84e93e601d
adriley <adriley@outlook.com>
2017-10-06 03:28:11 -0300

    tt
    tt

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+
+import { AngularFireDatabase, FirebaseListObservable } from  'angularfire2/database-deprecated/'; //'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  user = null;
+  cuisines;//: any[] ; //FirebaseListObservable<any[]>;
+  restaurant;//:any[];
+
+ private subscription;
+  constructor( private db: AngularFireDatabase) {
+   /* this.auth.getAuthState().subscribe(
+ //     (user) => this.user = user);
+   //     this.cuisines = this.db.list('/cuisines');
+
+
+    )*/
+    // console.log(db);
+
+
+
+
+
+   }
+    loginWithGoogle() {
+   // this.auth.loginWithGoogle();
+  }
+ngOnInit() {
+  this.cuisines = this.db.list('/cuisines');
+   this.restaurant = this.db.list('/restaurant');
+ // this.subscription= this.db.list('/restaurant').subscribe(x=>{ this.restaurant = x;})
+}
+  /*  ngOnDestroy() {
+      //Called once, before the instance is destroyed.
+      //Add 'implements OnDestroy' to the class.
+      this.subscription.unsubdescribe();
+
+    }*/
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+ export const environment = {
+  production: false,
+  firBConfig: {
+  apiKey: 'AIzaSyC6sTYsioDQUQpJIcGVJryOsuiJePmUUkA',
+  authDomain: 'devgeral-359a0.firebaseapp.com',
+  databaseURL: 'https://devgeral-359a0.firebaseio.com',
+  projectId: 'devgeral-359a0',
+  storageBucket: 'devgeral-359a0.appspot.com',
+  messagingSenderId: '515689976964'
+  }
+};
