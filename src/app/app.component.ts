b1c22afcd4d805a9fb8f4b9b9345f67b5383d1b9
adriley <adriley@outlook.com>
2017-10-10 19:46:13 -0300

    continuando
    continuando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+
 import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './shared/auth.service';
-
+//import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated/';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated/'; //'angularfire2/database';
 import * as firebase from 'firebase/app';
 import 'rxjs/add/operator/map';
@@ -14,38 +15,43 @@ export class AppComponent implements OnInit {
   user = null;
   cuisines: FirebaseListObservable<any[]>;
   restaurants: Observable<any[]>;
+  existe;
 
- private subscription;
-  constructor( private db: AngularFireDatabase) {
-   /* this.auth.getAuthState().subscribe(
- //     (user) => this.user = user);
-   //     this.cuisines = this.db.list('/cuisines');
+  private subscription;
+  constructor(private db: AngularFireDatabase) {
+    /* this.auth.getAuthState().subscribe(
+  //     (user) => this.user = user);
+    //     this.cuisines = this.db.list('/cuisines');
 
-    )*/
+     )*/
     // console.log(db);
-   }
-
-    loginWithGoogle() {
-   // this.auth.loginWithGoogle();
   }
-ngOnInit() {
-  this.cuisines = this.db.list('/cuisines');
-  this.restaurants = this.db.list('/restaurants').map(restaurants => {
-   console.log('before map:', restaurants);
-   restaurants.map(restaurant=>{
-     //restaurant.cuisineType = this.db.object('/cusines' + restaurant.cuisines)
-     restaurant.featureTypes =[];
-     for(var f in restaurant.features)
-      restaurant.featureTypes.push(this.db.object('/features'+ f));
-// console.log(restaurants);
+
+  loginWithGoogle() {
+    // this.auth.loginWithGoogle();
+  }
+  ngOnInit() {
+   this.cuisines = this.db.list('/cuisines');
+  this.restaurants = this.db.list('/restaurants')
+   /*     .map(restaurants => {
+        restaurants.map(restaurant => {
+        //restaurant.cuisineType = this.db.object('/cusines' + restaurant.cuisines)
+         restaurant.featureTypes = [];
+       for (var f in restaurant.features)
+           restaurant.featureTypes.push(this.db.object('/features' + f));
+        console.log( restaurant.featureTypes.push(this.db.object('/features' + f)));
+      });
+      ///  console.log("After map",restaurants);
+      return restaurants;
+    });
+    // this.subscription= this.db.list('/restaurant').subscribe(x=>{ this.restaurant = x;})
+*/
+
+  this.existe = this.db.object('/restaurants/1/feature/1');
+   this.existe.subscribe(x=>{console.log(x);
    });
-  ///  console.log("After map",restaurants);
-   return restaurants;
 
-  });
- // this.subscription= this.db.list('/restaurant').subscribe(x=>{ this.restaurant = x;})
-
-}
+  }
   /*  ngOnDestroy() {
       //Called once, before the instance is destroyed.
       //Add 'implements OnDestroy' to the class.
